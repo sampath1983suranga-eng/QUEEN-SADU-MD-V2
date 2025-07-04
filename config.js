b3250cb2e3bcad6dd15e8380db037b69d88fa730
dinesh madusanka <dineshofcai@gmail.com>
2025-07-04 21:53:47 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ SESSION_ID: process.env.SESSION_ID || "w3FkVLaL#KDg-i44g1-frMdPS1ne8xL99akZyzJLx
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
-AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "true",
+AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
 // make true if you want auto reply on status 
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
 // make true if you want auto reply on status 
