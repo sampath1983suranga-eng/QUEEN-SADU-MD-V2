41d00094f857be4106580e0015a55eeeded478b7
dinesh madusanka <dineshofcai@gmail.com>
2025-07-10 20:44:43 +0530

    Add files via upload

--- command.js
+++ command.js
@@ -5,17 +5,16 @@ function cmd(info, func) {
     data.function = func;
     if (!data.dontAddCommandList) data.dontAddCommandList = false;
     if (!info.desc) info.desc = '';
-    if (!data.fromMe) data.fromMe = false; // <<< default to false
-    if (!info.category) info.category = 'misc';
-    if (!info.filename) info.filename = "Not Provided";
+    if (!data.fromMe) data.fromMe = false;
+    if (!info.category) data.category = 'misc';
+    if(!info.filename) data.filename = "Not Provided";
     commands.push(data);
     return data;
 }
-
 module.exports = {
     cmd,
-    AddCommand: cmd,
-    Function: cmd,
-    Module: cmd,
+    AddCommand:cmd,
+    Function:cmd,
+    Module:cmd,
     commands,
 };
