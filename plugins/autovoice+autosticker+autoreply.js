158a2df19ff36acae73ad1be71d179661c97c522
dinesh madusanka <dineshofcai@gmail.com>
2025-07-13 01:06:16 +0530

    Update autovoice+autosticker+autoreply.js

--- plugins/autovoice+autosticker+autoreply.js
+++ plugins/autovoice+autosticker+autoreply.js
@@ -71,4 +71,24 @@ async (conn, mek, m, { from, body, isOwner }) => {
             }
          } 
    );
-//always offline
+
+
+//quiz_answers.json
+cmd({
+  on: "body"
+},    
+async (conn, mek, m, { from, body, isOwner }) => {
+    const filePath = path.join(__dirname, '../data/quiz_answers.json');
+    const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+    for (const text in data) {
+        if (body.toLowerCase() === text.toLowerCase()) {
+            
+            if (config.AUTO_REPLY === 'true') {
+                //if (isOwner) return;        
+                await m.reply(data[text])
+            
+            }
+        }
+    }                
+});
+// ofline all
