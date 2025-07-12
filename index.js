17645c1ef6c12ce39ee15f893d51cb126973a165
dinesh madusanka <dineshofcai@gmail.com>
2025-07-12 21:41:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -187,9 +187,8 @@ const port = process.env.PORT || 9090;
   const isMe = botNumber.includes(senderNumber)
   const isOwner = ownerNumber.includes(senderNumber) || isMe
   const botNumber2 = await jidNormalizedUser(conn.user.id);
-  const groupMetadata = isGroup ? await conn.groupMetadata(from).catch(e => {}) : ''
-  const groupName = isGroup ? groupMetadata.subject : ''
-  const participants = isGroup ? await groupMetadata.participants : ''
+  const groupMetadata = isGroup ? await conn.groupMetadata(from).catch(e => {}) : '' 
+  const groupName = (isGroup && groupMetadata && groupMetadata.subject) ? groupMetadata.subject : '';
   const groupAdmins = isGroup ? await getGroupAdmins(participants) : ''
   const isBotAdmins = isGroup ? groupAdmins.includes(botNumber2) : false
   const isAdmins = isGroup ? groupAdmins.includes(sender) : false
