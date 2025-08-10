c9ac56538d4d5eea06267794a821335d09342116
sampath1983suranga-eng <sampath1983suranga@gmail.com>
2025-08-10 14:21:22 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "QUEEN-SADU-MD&06sgSDjJ#s5TH2D9SeYLrtwJWONzEkj_vUdiS6DwZL0Q0Ax6q5Fk",
+SESSION_ID: process.env.SESSION_ID || "cVYVWKaS#tY2vh_2-kHxi_rCAVTic9RNFcYHaia2Uj2wpGpbkFlY",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
@@ -27,7 +27,7 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "94781536595",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "94706042889",
 // add your bot owner number
 OWNER_NAME: process.env.OWNER_NAME || "ᴍʀ ᴅɪɴᴇꜱʜ ᴏꜰᴄ",
 // add bot owner name
